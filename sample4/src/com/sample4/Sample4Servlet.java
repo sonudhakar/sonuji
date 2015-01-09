358af2de73f401d949e9b051167e39bbc3f6c57a
sonudhakar <sonu.dhakar@a-cti.com>
2015-01-09 21:37:48 +0530

    this is my first commit

---
+++ sample4/src/com/sample4/Sample4Servlet.java
@@ -0,0 +1,13 @@
+package com.sample4;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Sample4Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
